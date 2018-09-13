0197f495380ec70c48de2a98114daf5e1bad0f59
David Hernandez <brodave318@gmail.com>
2018-09-13 12:37:47 -0700

    First commit index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+/* 
+ * Primary file for the API
+ */
+
+ // Dependencies
+ const server = require('./lib/server');
+ const workers = require('./lib/workers');
+ // Declare the app
+ const app = {};
+ // Init function
+ app.init = function() {
+	// Start the server
+	server.init();
+	// Start the workers
+	workers.init();
+ };
+ // Execute the function
+ app.init();
+ // Export the app
+ module.exports = app;
